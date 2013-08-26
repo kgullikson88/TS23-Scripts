f32f78147d3d4420f6c51b735891daa4fc56a9b9
kgullikson <kgullikson@fc0a986b-1a2a-4d67-b47a-a64e0a7f0230>
2013-08-26 15:41:53 +0000

    Using the HET version of ApplyTelluricCorrection

--- ApplyTelluricCorrection.py
+++ ApplyTelluricCorrection.py
@@ -5,7 +5,6 @@ from scipy.interpolate import InterpolatedUnivariateSpline as interp
 import matplotlib.pyplot as plt
 import DataStructures
 import os
-import FitTellurics_McDonald
 import FindContinuum
 import numpy
 
@@ -29,22 +28,12 @@ def ReadCorrectedFile(fname):
 
 def Correct(original, corrected, offset=None):
   #Read in the data and model
-  original_orders = FitsUtils.MakeXYpoints(original, errors=2)
+  original_orders = FitsUtils.MakeXYpoints(original, extensions=True, x="wavelength", y="flux", errors="error", cont="continuum")
   corrected_orders, corrected_headers = ReadCorrectedFile(corrected)
+  print len(original_orders), len(corrected_orders)
   if offset == None:
     offset = len(original_orders) - len(corrected_orders)
-
-  #Find and read in blaze function (MUST BE IN CURRENT DIRECTORY!)
-  files = os.listdir("./")
-  blazefile = [fname for fname in files if fname.startswith("BLAZE")][0]
-  blaze_orders = FitsUtils.MakeXYpoints(blazefile, errors=2)
-  blaze_functions = []
-  for order in blaze_orders:
-    blaze_functions.append( interp(order.x, order.y) )
-
-  for i in range(len(original_orders)):
-    original_orders[i].y /= blaze_functions[i](original_orders[i].x)
-
+  offset = 0
   for i in range(offset, len(original_orders)):
     data = original_orders[i]
     data.cont = FindContinuum.Continuum(data.x, data.y)
@@ -55,35 +44,91 @@ def Correct(original, corrected, offset=None):
     except IndexError:
       model = DataStructures.xypoint(x=data.x, y=numpy.ones(data.x.size))
       print "Warning!!! Telluric Model not found for order %i" %i
+
+    #plt.plot(data.x, data.y/data.cont)
+    #plt.plot(model.x, model.y)
+    #plt.show()
+    if model.size() < data.size():
+      left = numpy.searchsorted(data.x, model.x[0])
+      right = numpy.searchsorted(data.x, model.x[-1])
+      if right < data.size():
+        right += 1
+      data = data[left:right]
+    elif model.size() > data.size():
+      sys.exit("Error! Model size (%i) is larger than data size (%i)" %(model.size(), data.size()))
+
+    badindices = numpy.where(numpy.logical_or(data.y <= 0, model.y < 0.05))[0]
+    model.y[badindices] = data.y[badindices]
+    
     data.y /= model.y
     original_orders[i] = data.copy()
   return original_orders
 
-if __name__ == "__main__":
-  for number in sys.argv[1:]:
-    #Number is for for e.g. KG11186.fits
-    original = "KG%s.fits" %number
-    corrected = "Corrected_%s-0.fits" %number
-    original = "%s.fits" %number
-    corrected = "Corrected_%s.fits" %number
-    #original = "HIP_16147.fits"
-    #corrected = "HIP_16147-0.fits"
-    print original
-    print corrected
+
+
+
+
+def main1():
+  if len(sys.argv) > 2:
+    original = sys.argv[1]
+    corrected = sys.argv[2]
+  
     outfilename = "%s_telluric_corrected.fits" %(original.split(".fits")[0])
     print "Outputting to %s" %outfilename
 
-    try:
-      corrected_orders = Correct(original, corrected, offset=2)
-    except IOError:
-      continue
+    corrected_orders = Correct(original, corrected, offset=None)
+
+    column_list = []
     for i, data in enumerate(corrected_orders):
+      plt.plot(data.x, data.y/data.cont)
       #Set up data structures for OutputFitsFile
       columns = {"wavelength": data.x,
                  "flux": data.y,
                  "continuum": data.cont,
                  "error": data.err}
-      if i == 0:
-        FitTellurics_McDonald.OutputFitsFile(columns, original, outfilename, mode="new")
+      column_list.append(columns)
+    FitsUtils.OutputFitsFileExtensions(column_list, original, outfilename, mode="new")
+    
+    plt.show()
+
+  else:
+    allfiles = os.listdir("./")
+    corrected_files = [f for f in allfiles if "Corrected_" in f and f.endswith(".fits")]
+    #original_files = [f for f in allfiles if any(f in cf for cf in corrected_files)]
+    hip_files = [f for f in allfiles if "HIP_" in f and f.endswith("-0.fits")]
+
+    for hip in hip_files:
+      if any([hip.replace("-0", "-1") in f for f in corrected_files]):
+        original = hip
+        corrected = "Corrected_%s" %(hip.replace("-0", "-1"))
+        print corrected, original
+      
+        outfilename = "%s_telluric_corrected.fits" %(original.split(".fits")[0])
+        print "Outputting to %s" %outfilename
+
+        corrected_orders = Correct(original, corrected, offset=None)
+
+        column_list = []
+        for i, data in enumerate(corrected_orders):
+          plt.plot(data.x, data.y/data.cont)
+          #Set up data structures for OutputFitsFile
+          columns = {"wavelength": data.x,
+                     "flux": data.y,
+                     "continuum": data.cont,
+                     "error": data.err}
+          column_list.append(columns)
+        FitsUtils.OutputFitsFileExtensions(column_list, original, outfilename, mode="new")
+        
+        plt.title(original)
+        plt.xlabel("Wavelength (nm)")
+        plt.ylabel("Flux")
+        plt.show()
+
       else:
-        FitTellurics_McDonald.OutputFitsFile(columns, outfilename, outfilename)
+        print "No Correction file found for file %s" %hip
+
+
+
+
+if __name__ == "__main__":
+  main1()
