bbbfbf590d6d939d8f54d8a1ac043a85ceeb71db
kgullikson88 <kevin.gullikson@gmail.com>
2014-12-08 16:00:21 -0600

    Fixed modeldir

--- Search_temp.py
+++ Search_temp.py
@@ -14,13 +14,13 @@ badregions = [[567.5, 575.5],
               # [655, 657],  # H alpha
               # [485, 487],  #H beta
               # [433, 435],  #H gamma
-              #[409, 411],  #H delta
+              # [409, 411],  #H delta
               #[396, 398],  #H epsilon
               #[388, 390],  #H zeta
 ]
 
 if "darwin" in sys.platform:
-    modeldir = "/Volumes/DATADRIVE/Stellar_Models/PHOENIX/Stellar/Vband/"
+    modeldir = "/Volumes/DATADRIVE/Stellar_Models/Sorted/Stellar/Vband/"
 elif "linux" in sys.platform:
     modeldir = "/media/FreeAgent_Drive/SyntheticSpectra/Sorted/Stellar/Vband/"
 else:
