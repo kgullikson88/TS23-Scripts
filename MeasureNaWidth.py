759ab96996fabb929c3e94c88821eafe44772cb5
kgullikson88 <kevin.gullikson@gmail.com>
2014-12-08 16:00:40 -0600

    Fixed formatting issues.

--- MeasureNaWidth.py
+++ MeasureNaWidth.py
@@ -6,24 +6,21 @@ The relationship from EW to E(B-V) is taken from
 Poznanski et al. 2012 MNRAS, 426, 1465
 """
 
-from scipy.optimize import leastsq
-from scipy.interpolate import UnivariateSpline
 import sys
 import os
+import FittingUtilities
 
+from scipy.interpolate import UnivariateSpline
 import matplotlib.pyplot as plt
 import numpy as np
-import FittingUtilities
 import DataStructures
 from astropy.io import fits
-
-import SpectralTypeRelations
 import HelperFunctions
 import pyspeckit
-import Smooth
 
 
-#ordernum = 30  #The order number with the Sodium lines in it
+
+# ordernum = 30  #The order number with the Sodium lines in it
 D1_wave = 589.592
 D2_wave = 588.995
 
@@ -32,18 +29,18 @@ plt.ion()
 
 
 def FindOrderNum(orders, wavelength):
-  """
-    Given a list of xypoint orders and
-    another list of wavelengths, this
-    finds the order numbers with the
-    requested wavelengths
-  """
-  num = 0
-  for i, order in enumerate(orders):
-    if order.x[0] < wavelength and order.x[-1] > wavelength:
-      num = i
-      break
-  return num
+    """
+      Given a list of xypoint orders and
+      another list of wavelengths, this
+      finds the order numbers with the
+      requested wavelengths
+    """
+    num = 0
+    for i, order in enumerate(orders):
+        if order.x[0] < wavelength and order.x[-1] > wavelength:
+            num = i
+            break
+    return num
 
 
 def GetReddening(D1_ew, D2_ew):
@@ -53,14 +50,12 @@ def GetReddening(D1_ew, D2_ew):
 
 
 def Measure3(filename, figure, title):
-  orders = HelperFunctions.ReadExtensionFits(filename)
-  ordernum = FindOrderNum(orders, 589)
-  order = orders[ordernum]
-  left = numpy.searchsorted(order.x, 588)
-  right = numpy.searchsorted(order.x, 590.5)
-  order = order[left:right]
-
-    #Get the line positions
+    orders = HelperFunctions.ReadExtensionFits(filename)
+    ordernum = FindOrderNum(orders, 589)
+    order = orders[ordernum]
+    left = np.searchsorted(order.x, 588)
+    right = np.searchsorted(order.x, 590.5)
+    order = order[left:right]  # Get the line positions
     order.y /= order.cont
     order.err /= order.cont
     xgrid = np.linspace(order.x[0], order.x[-1], order.size())
@@ -142,21 +137,19 @@ def Measure3(filename, figure, title):
 
 
 def Measure2(filename, figure, title):
-  """
-  Given a filename, this will plot the appropriate order
-  and help the user measure the equivalent width of
-  both sodium D lines. Unlike the 'Measure' function,
-  this one just performs a simpson rule interpolation
-  to integrate under the curve
-  """
-  orders = HelperFunctions.ReadExtensionFits(filename)
-  ordernum = FindOrderNum(orders, 589)
-  order = orders[ordernum]
-  left = numpy.searchsorted(order.x, 588)
-  right = numpy.searchsorted(order.x, 590.5)
-  order = order[left:right]
-
-    # Get the Na line positions
+    """
+    Given a filename, this will plot the appropriate order
+    and help the user measure the equivalent width of
+    both sodium D lines. Unlike the 'Measure' function,
+    this one just performs a simpson rule interpolation
+    to integrate under the curve
+    """
+    orders = HelperFunctions.ReadExtensionFits(filename)
+    ordernum = FindOrderNum(orders, 589)
+    order = orders[ordernum]
+    left = np.searchsorted(order.x, 588)
+    right = np.searchsorted(order.x, 590.5)
+    order = order[left:right]  # Get the Na line positions
     order.y /= order.cont
     xgrid = np.linspace(order.x[0], order.x[-1], order.size())
     order = FittingUtilities.RebinData(order, xgrid)
@@ -192,24 +185,24 @@ def Measure2(filename, figure, title):
         fcn = UnivariateSpline(order2.x, order2.y, s=smooth_value)
         order.cont = fcn(order.x)
 
-    #Plot this value to make sure it looks fine
-    ax = figure.add_subplot(111)
-    plt.cla()
-    ax.plot(order.x, order.y)
-    ax.plot(order.x, order.cont)
-    plt.show()
-    inp = raw_input("Continuum fit okay? ")
-    if inp.strip() == "":
-      done = True
-    elif "+" in inp:
-      smooth_value += 1e-4
-      done = False
-    elif "-" in inp:
-      if smooth_value - 1e-4 <=0:
-        smooth_value /= 2.0
-      else:
-        smooth_value -= 1e-4
-      done = False
+        # Plot this value to make sure it looks fine
+        ax = figure.add_subplot(111)
+        plt.cla()
+        ax.plot(order.x, order.y)
+        ax.plot(order.x, order.cont)
+        plt.show()
+        inp = raw_input("Continuum fit okay? (Hit '+', '-', or enter if okay)")
+        if inp.strip() == "":
+            done = True
+        elif "+" in inp:
+            smooth_value += 1e-4
+            done = False
+        elif "-" in inp:
+            if smooth_value - 1e-4 <= 0:
+                smooth_value /= 2.0
+            else:
+                smooth_value -= 1e-4
+            done = False
 
     x = pyspeckit.units.SpectroscopicAxis(order.x, units='nm')
     spec = pyspeckit.Spectrum(data=order.y / order.cont, xarr=x, error=order.err / order.cont, units='nm')
@@ -221,42 +214,41 @@ def Measure2(filename, figure, title):
     plt.show()
     done = raw_input("hit enter ")
 
-  start1, end1 = spec.baseline.xclicks[1], spec.baseline.xclicks[2]
-  start2, end2 = spec.baseline.xclicks[3], spec.baseline.xclicks[4]
-  start1 = spec.xarr[start1]
-  end1 = spec.xarr[end1]
-  start2 = spec.xarr[start2]
-  end2 = spec.xarr[end2]
-  print spec.baseline.xclicks
-  print start1, end1
-  print start2, end2
+    start1, end1 = spec.baseline.xclicks[1], spec.baseline.xclicks[2]
+    start2, end2 = spec.baseline.xclicks[3], spec.baseline.xclicks[4]
+    start1 = spec.xarr[start1]
+    end1 = spec.xarr[end1]
+    start2 = spec.xarr[start2]
+    end2 = spec.xarr[end2]
+    print spec.baseline.xclicks
+    print start1, end1
+    print start2, end2
 
-  #Find the equivalent width for both lines
-  ews = []
-  for line in [[start1, end1], [start2, end2]]:
-    xmin=numpy.searchsorted(order.x, line[0])
-    xmax=numpy.searchsorted(order.x, line[1])
-    ew = spec.specfit.EQW(xmin=xmin, xmax=xmax, fitted=False, plot=True)
-    ews.append(ew*10)  #Save the equivalent width in angstroms
-  done = raw_input("hit enter ")
-  print "\n\n"
+    # Find the equivalent width for both lines
+    ews = []
+    for line in [[start1, end1], [start2, end2]]:
+        xmin = np.searchsorted(order.x, line[0])
+        xmax = np.searchsorted(order.x, line[1])
+        ew = spec.specfit.EQW(xmin=xmin, xmax=xmax, fitted=False, plot=True)
+        ews.append(ew * 10)  #Save the equivalent width in angstroms
+    done = raw_input("hit enter ")
+    print "\n\n"
 
-  return ews, GetReddening(*ews)
-
+    return ews, GetReddening(*ews)
 
 
 def Measure(filename, figure, title):
-  """
-  Given a filename, this will plot the appropriate order
-  and help the user measure the equivalent width of
-  both sodium D lines
-  """
-  orders = HelperFunctions.ReadExtensionFits(filename)
-  ordernum = FindOrderNum(orders, 589)
-  order = orders[ordernum]
-  left = numpy.searchsorted(order.x, 588)
-  right = numpy.searchsorted(order.x, 590.5)
-  order = order[left:right]
+    """
+    Given a filename, this will plot the appropriate order
+    and help the user measure the equivalent width of
+    both sodium D lines
+    """
+    orders = HelperFunctions.ReadExtensionFits(filename)
+    ordernum = FindOrderNum(orders, 589)
+    order = orders[ordernum]
+    left = np.searchsorted(order.x, 588)
+    right = np.searchsorted(order.x, 590.5)
+    order = order[left:right]
 
     x = pyspeckit.units.SpectroscopicAxis(order.x, units='nm')
     spec = pyspeckit.Spectrum(data=order.y / order.cont, xarr=x, error=order.err / order.cont, units='nm')
@@ -303,17 +295,16 @@ def Measure(filename, figure, title):
 
 
 if __name__ == "__main__":
-  #Read in the lines in the current log file
-  logfilename = "%s/Dropbox/School/Research/AstarStuff/TargetLists/Na_Params.csv" %os.environ['HOME']
-
-    #Parse command line arguments
+    # Read in the lines in the current log file
+    logfilename = "%s/Dropbox/School/Research/AstarStuff/TargetLists/Na_Params.csv" % os.environ[
+        'HOME']  #Parse command line arguments
     fileList = []
     for arg in sys.argv[1:]:
         fileList.append(arg)
 
-  #For each file, determine the EW and E(B-V)
-  fig = plt.figure()
-  for fname in fileList:
+# For each file, determine the EW and E(B-V)
+fig = plt.figure()
+for fname in fileList:
     #First, check to make sure that this target was not already measured
     logfile = open(logfilename, "r")
     lines = logfile.readlines()
@@ -324,26 +315,26 @@ if __name__ == "__main__":
     print starname
     #Check if this star already has an entry in the logfile
     for linenum, line in enumerate(lines):
-      star = line.split("|")[0].strip()
-      if star.lower() == starname.lower():
-        measure = False
-        print "%s found in the master logfile." %starname
-        inp = raw_input("Do you want to overwrite (y/n)? ")
-        if inp.lower() == "y":
-          measure = True
-          overwrite = linenum
+        star = line.split("|")[0].strip()
+        if star.lower() == starname.lower():
+            measure = False
+            print "%s found in the master logfile." % starname
+            inp = raw_input("Do you want to overwrite (y/n)? ")
+            if inp.lower() == "y":
+                measure = True
+                overwrite = linenum
 
-        if measure:
-            ews, reddening = Measure2(fname, fig, starname)
-            print ews, reddening
+            if measure:
+                ews, reddening = Measure2(fname, fig, starname)
+                print ews, reddening
 
-            outline = "%s | %.4f | %.4f | %.4f\n" % (starname, ews[0], ews[1], reddening)
-            if overwrite > len(lines):
-                lines.append(outline)
-            else:
-                lines[overwrite] = outline
+                outline = "%s | %.4f | %.4f | %.4f\n" % (starname, ews[0], ews[1], reddening)
+                if overwrite > len(lines):
+                    lines.append(outline)
+                else:
+                    lines[overwrite] = outline
 
-      #Finally, output the new logfile
-      logfile = open(logfilename, "w")
-      logfile.writelines(lines)
-      logfile.close()
+        # Finally, output the new logfile
+        logfile = open(logfilename, "w")
+        logfile.writelines(lines)
+        logfile.close()
