3c7001401ac69254a597e3e7ef7f43fa44ebeb3f
kgullikson88 <kevin.gullikson@gmail.com>
2015-01-14 13:31:58 -0600

    Added function to edit fits files.

--- ccdlist.py
+++ ccdlist.py
@@ -6,7 +6,7 @@ from astropy.io import fits as pyfits
 if __name__ == "__main__":
     for fname in sys.argv[1:]:
         hdulist = pyfits.open(fname)
-	header = hdulist[0].header
-        #header = pyfits.getheader(fname)
-        print fname, header["OBJECT"], len(hdulist)
+        header = hdulist[0].header
+        # header = pyfits.getheader(fname)
+        print fname, header["OBJECT"], header['DATE-OBS'], header['UT']
 
---
+++ fitsedit.py
@@ -0,0 +1,28 @@
+import sys
+
+from astropy.io import fits
+
+
+if __name__ == '__main__':
+    header_kws = {}
+    file_list = []
+    for arg in sys.argv[1:]:
+        if "=" in arg:
+            s = arg.partition("=")
+            key = s[0]
+            value = s[-1]
+            try:
+                value = float(value)
+            except ValueError:
+                pass
+            header_kws[key] = value
+        else:
+            file_list.append(arg)
+
+    for filename in file_list:
+        hdulist = fits.open(filename, mode='update')
+        for key in header_kws.keys():
+            hdulist[0].header[key] = value
+
+        hdulist.flush()
+        hdulist.close()
